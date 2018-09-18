f5fac7f0254442a3fde729d9965f0bf155a9e234
feramirez5725 <feramirez5725@7127R2S8.STUDENT.ad.fgcu.edu>
2018-09-18 10:09:46 -0400

    Progress

---
+++ Hello World/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Creating a connect four game
+		// Dimensions 7 width X 6 height.
+		// Total of 42 underscores
+		System.out.println("Hello players");
+		
+		
+		
+		
+		
+	}
+}
